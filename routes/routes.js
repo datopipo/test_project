2991169c14b26d259aeb75a87ac60b5a211431b4
david <datopipo@gmail.com>
2022-11-27 05:34:03 -0800

    Create routes.js

---
+++ routes/routes.js
@@ -0,0 +1,134 @@
+const router = require('express').Router()
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+
+router.post('/sign-up', async (req, res) => {
+
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(req.body.password, salt)
+    const user = new User({
+        email: req.body.email,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        birthday: req.body.birthday,
+        password: hashedPassword,
+    })
+
+    const result = await user.save()
+
+    const {password, ...data} = await result.toJSON()
+
+    res.send(data)
+})
+
+router.post('/login', async (req, res) => {
+    const user = await User.findOne({email: req.body.email})
+    if (!user) {
+        return res.status(400).send({
+            message: 'invalid credentials'
+        })
+    }
+
+    if (!await bcrypt.compare(req.body.password, user.password)) {
+        return res.status(400).send({
+            message: 'invalid credentials'
+        })
+    }
+    const token = jwt.sign({_id: user._id}, "secret")
+
+    res.cookie('jwt', token, {
+        httpOnly: true,
+        maxAge: 24 * 60 * 60 * 100
+    })
+    res.send({
+        message: 'success'
+    })
+})
+
+router.get('/user', async (req, res) => {
+    try {
+        const cookie = req.cookies['jwt']
+
+        const claims = jwt.verify(cookie, 'secret')
+
+        if (!claims) {
+            return res.status(401).send({
+                message: 'unauthenticated'
+            })
+        }
+
+        const user = await User.findOne({_id: claims._id})
+
+        const {password, ...data} = await user.toJSON()
+
+        res.send(data)
+    } catch (e) {
+        return res.status(401).send({
+            message: 'unauthenticated'
+        })
+    }
+})
+
+router.post('/logout', (req, res) => {
+    res.cookie('jwt', '', {maxAge: 0})
+    res.send({
+        message: 'success'
+    })
+})
+
+router.put('/users', async (req, res) => {
+    try {
+        const cookie = req.cookies['jwt']
+
+        const claims = jwt.verify(cookie, 'secret')
+
+        if (!claims) {
+            return res.status(401).send({
+                message: 'unauthenticated'
+            })
+        }
+        const update = ({
+            email: req.body.email,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            birthday: req.body.birthday,
+        })
+
+        const user = await User.findOneAndUpdate({_id: claims._id},update)
+
+        return res.status(200).send({
+            message: 'updated'
+        })
+    } catch (e) {
+        return res.status(401).send({
+            message: 'unauthenticated'
+        })
+    }
+})
+
+
+router.delete('/users', async (req, res) => {
+    try {
+        const cookie = req.cookies['jwt']
+        const claims = jwt.verify(cookie, 'secret')
+
+        if (!claims) {
+            return res.status(401).send({
+                message: 'unauthenticated'
+            })
+        }
+
+        const user = await User.findOneAndDelete({_id: claims._id})
+
+        return res.status(200).send({
+            message: 'deleted'
+        })
+    } catch (e) {
+        return res.status(401).send({
+            message: 'unauthenticated'
+        })
+    }
+
+})
+module.exports = router;
