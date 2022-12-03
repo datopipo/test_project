41225696b887f268c8151284c5d89d5819f66093
david <datopipo@gmail.com>
2022-12-03 02:10:52 -0800

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,28 +1,30 @@
 const jwt = require("jsonwebtoken");
-const mongoose = require("mongoose");
-const auth = require("../middlware/auth");
 
+//const config = process.env;
 
-function checkToken  (req, res, next) {
-    let token = req.headers["x-access-token"] || req.headers["authorization"];
-    if (token.startsWith('Bearer ')) {
-        token = token.split(7, token.length);
+function verifyToken(req,res,next){
+    //Auth header value = > send token into header
+
+    const bearerHeader = req.headers['authorization'];
+    //check if bearer is undefined
+    if(typeof bearerHeader !== 'undefined'){
+
+        //split the space at the bearer
+        const bearer = bearerHeader.split(' ');
+        //Get token from string
+        const bearerToken = bearer[1];
+
+        //set the token
+        req.token = bearerToken;
+
+        //next middleweare
+        next();
+
+    }else{
+        //Fobidden
+        res.sendStatus(403);
     }
-    if (token) {
-        jwt.verify(token, 'secret', (err, decoded) => {
-                if (err) {
-                    return res.json({
-                        success: false,
-                        message: "token not right"
-                    });
-                } else {
 
-                    req.decoded = decoded;
-                    next();
-                }
-            }
-        )
-    }}
+}
 
-module.exports = checkToken;
-
+module.exports = verifyToken;
